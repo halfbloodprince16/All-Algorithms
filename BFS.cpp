48dc3cd64609265981263f4ceccaa2000220cd94
ilikegirls <26187718+halfbloodprince16@users.noreply.github.com>
2019-01-28 23:02:19 +0530

    Add files via upload

--- BFS.cpp
+++ BFS.cpp
@@ -1,44 +1,57 @@
-#include"bits/stdc++.h"
+#include "bits/stdc++.h"
 using namespace std;
 
-std::vector<pair<int,int>> v;
+std::vector<pair<int,int>>v;
 std::vector<pair<int,int>>::iterator itr;
 std::queue<int>q;
 std::map<int,bool>m;
 
 void bfs()
 {
-	int i,j,k;
-	q.push(2);//choose a head
-
-	while(!q.empty())
-	{
-		k = q.front(); q.pop();
-		cout<<k<<" "; m[k] = true;
-
-		for(itr = v.begin() ; itr!= v.end() ;itr++)
-		{
-			if(itr->first == k && m[itr->second] == false)
-			{
-				q.push(itr->second);
-			}
-		}
-	}
+    q.push(1);
+    int k;
+    cout<<"1->";
+    while(!q.empty())
+    {
+        k = q.front(); 
+        q.pop();
+        m[k] = true;
+        for(itr=v.begin();itr!=v.end();itr++)
+        {
+            if(itr->first == k && m[itr->second]==false)
+            {
+                q.push(itr->second);
+                cout<<itr->second<<"->";
+                m[itr->second] = true;
+            }
+        }
+    }
 }
 
-int main(int argc, char const *argv[])
+int main() 
 {
-	v.push_back(make_pair(0,1));
-	v.push_back(make_pair(0,2));
 	v.push_back(make_pair(1,2));
-	v.push_back(make_pair(2,0));
-	v.push_back(make_pair(2,3));
-	v.push_back(make_pair(3,3));
-
-	m.insert(pair<int,bool>(0,false));
+	v.push_back(make_pair(1,3));
+	v.push_back(make_pair(2,1));
+	v.push_back(make_pair(2,4));
+	v.push_back(make_pair(2,5));
+	v.push_back(make_pair(3,1));
+	v.push_back(make_pair(3,5));
+	v.push_back(make_pair(4,2));
+	v.push_back(make_pair(4,5));
+	v.push_back(make_pair(4,6));
+	v.push_back(make_pair(5,2));
+	v.push_back(make_pair(5,3));
+	v.push_back(make_pair(5,4));
+	v.push_back(make_pair(5,6));
+	v.push_back(make_pair(6,4));
+	v.push_back(make_pair(6,5));
+	
 	m.insert(pair<int,bool>(1,false));
 	m.insert(pair<int,bool>(2,false));
 	m.insert(pair<int,bool>(3,false));
+	m.insert(pair<int,bool>(4,false));
+	m.insert(pair<int,bool>(5,false));
+	m.insert(pair<int,bool>(6,false));
 	bfs();
-	return 0;
 }
--- DFS.cpp
+++ DFS.cpp
@@ -1,49 +1,63 @@
-#include"bits/stdc++.h"
+#include "bits/stdc++.h"
 using namespace std;
 
-std::vector<pair<int,int>> v;
+std::vector<pair<int,int>>v;
 std::vector<pair<int,int>>::iterator itr;
 std::stack<int>s;
 std::map<int,bool>m;
 
 void dfs()
 {
-	int i,j,flg=0;
-	s.push(2);//choose a head
-	cout<<s.top(); m[s.top()] = true;
-	while(!s.empty())
-	{
-		for(itr = v.begin() ; itr!= v.end() ;itr++)
-		{
-			if(itr->first == s.top() && m[itr->second] == false)
-			{
-				s.push(itr->second);
-				cout<<s.top(); m[s.top()] = true;
-				flg=1;
-				break;
-			}
-		}
-		if(flg==0)
-		{
-			s.pop();
-		}
-		flg=0;
-	}
+    s.push(1);
+    cout<<"1->";
+    m[1] = true;
+    int flg=0;
+    while(!s.empty())
+    {
+        for(itr=v.begin();itr!=v.end();itr++)
+        {
+            if(itr->first == s.top() && m[itr->second]==false)
+            {
+                s.push(itr->second);
+                cout<<itr->second<<"->"; 
+                m[itr->second]=true;
+                flg=1;
+                break;
+            }
+        }
+        
+        if(flg==0)
+        {
+            s.pop();   
+        }
+        flg=0;
+    }
 }
 
-int main(int argc, char const *argv[])
+int main() 
 {
-	v.push_back(make_pair(0,1));
-	v.push_back(make_pair(0,2));
 	v.push_back(make_pair(1,2));
-	v.push_back(make_pair(2,0));
-	v.push_back(make_pair(2,3));
-	v.push_back(make_pair(3,3));
-
-	m.insert(pair<int,bool>(0,false));
+	v.push_back(make_pair(1,3));
+	v.push_back(make_pair(2,1));
+	v.push_back(make_pair(2,4));
+	v.push_back(make_pair(2,5));
+	v.push_back(make_pair(3,1));
+	v.push_back(make_pair(3,5));
+	v.push_back(make_pair(4,2));
+	v.push_back(make_pair(4,5));
+	v.push_back(make_pair(4,6));
+	v.push_back(make_pair(5,2));
+	v.push_back(make_pair(5,3));
+	v.push_back(make_pair(5,4));
+	v.push_back(make_pair(5,6));
+	v.push_back(make_pair(6,4));
+	v.push_back(make_pair(6,5));
+	
 	m.insert(pair<int,bool>(1,false));
 	m.insert(pair<int,bool>(2,false));
 	m.insert(pair<int,bool>(3,false));
+	m.insert(pair<int,bool>(4,false));
+	m.insert(pair<int,bool>(5,false));
+	m.insert(pair<int,bool>(6,false));
 	dfs();
-	return 0;
 }
